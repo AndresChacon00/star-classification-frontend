d77b2827bcfc9c7d10f52e6966c50d092c468cf3
AndresChacon00 <andresroberto.c@gmail.com>
2024-12-08 13:59:11 -0400

    conexion con StarAPI

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,16 +1,17 @@
 'use client';
 
-import { formatDynamicAPIAccesses } from 'next/dist/server/app-render/dynamic-rendering';
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const FileUpload = () => {
   const [file, setFile] = useState(null);
+  const [response, setResponse] = useState(null);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
 
-  const handleSumit = (e) => {
+  const handleSumit = async (e) => {
     e.preventDefault();
     if (!file) return;
 
@@ -18,6 +19,17 @@ const FileUpload = () => {
     formData.append('file', file);
 
     // Aca debe ir la solicitud a StarAPI
+    try {
+      const res = await axios.post('http://127.0.0.1:5000/predict', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setResponse(res.data);
+      console.log('Respuesta de starAPI: ', res.data);
+    } catch (error) {
+      console.error('Error subiendo el archivo', error);
+    }
 
     console.log('Archivo subido', file);
   };
@@ -26,7 +38,7 @@ const FileUpload = () => {
     <div className="flex flex-col items-center">
       <form
         onSubmit={handleSumit}
-        className="flex flex-col items-center bg-white p-6 rounded-lg shadow-md"
+        className="flex flex-col items-center bg-zinc-900 p-6 rounded-lg shadow-md"
       >
         <input
           type="file"
@@ -36,7 +48,7 @@ const FileUpload = () => {
         />
         <button
           type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded"
+          className="bg-slate-500 text-white px-4 py-2 rounded"
         >
           Subir Archivo
         </button>
